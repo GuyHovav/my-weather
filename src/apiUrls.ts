7beb9c2cc403a7e5a2430ee5b6e7dd2692c035c8
Guy Hovav <guy.hovav@gmail.com>
2022-01-24 09:21:01 +0200

    change api port to 3050

--- src/apiUrls.ts
+++ src/apiUrls.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const apiPrefix = "http://localhost:3000"
+const apiPrefix = "http://localhost:3050"
 
 export async function getWeatherByGeoLocation(lan: number, lon: number) {
     return await axios.get(`${apiPrefix}/getWeatherByGeoLocation?lan=${lan}&lon=${lon}`)
