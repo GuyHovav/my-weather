ad3c6ac22288daddcbd32610412040c34c66c6aa
Guy Hovav <guy.hovav@gmail.com>
2022-01-24 09:20:41 +0200

    Add cors

--- server/main.js
+++ server/main.js
@@ -2,12 +2,16 @@ const express = require('express');
 
 
 const app = express()
-const port = 3000
+const port = 3050
 const axios = require('axios')
 
 const weatherApiKey = 'fO0eALTV1q3qroq0FMwx3bjrCoAXdRVG';
 const RESOURCE_URL = `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${weatherApiKey}`
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
